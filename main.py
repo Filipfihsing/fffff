0db3abeb947426cd4187815de59b2f765ae80bd4
Filipfihsing <switfilip@gmail.com>
2025-10-29 17:10:29 +0100

    Correct HTML and style issues in index.html
    Fix HTML structure and correct style tag errors.

--- main.py
+++ main.py
@@ -18,4 +18,58 @@ if word in meme_dict.keys():
 else:
     print("error")
     print(" takiego słowa nie ma")
+
+
+
+
+
+
+
+index.htme
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <stylr>
+        body
+         {
+            background-color: blanchedalomnd;
+         }   
+    </stylr>
+
+
+
+
+</head>
+<body>
+
+    <h1> style = "color:rgb(71, 62, 239)"</h1>
+    <h2>zdjęcie telefonu</h2>
+    <img src = ""alt = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSutn3lZ19XWRAjHXrfMyYiB3t9X5qTpQ5zOw&s">
+    <ol>
+        <li>Ustaw limity czasu spędzanego online/na swoich urządzeniach;
+        </li>
+        <li>Zróżnicuj swoje zainteresowania i zajęcia wykraczające poza wykorzystanie technologii;
+        </li>
+        <li>Szukaj nowych sposobów łączenia się z otaczającym Cię światem, aby zmniejszyć swoją zależność od mediów
+            społecznościowych i komunikatorów;
+        </li>
+        <li>Staraj się ograniczać czas spędzany w pracy przy komputerze i rób przerwy (jeśli to możliwe). Zmniejszy to
+            obciążenie oczu i pomoże oczyścić umysł przed następną sesją roboczą</li>
+    </ol>
+    
+</body>
+</html>
+
+
+
+
+
+
+
+
+
  
